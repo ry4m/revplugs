e6042d05b8bde599011e2ae778bc5928e7e0e933
ryamm <iwhsbomg2@gmail.com>
2026-09-09 21:21:20 +0300

    take

--- plugins/AvatarDownloader/src/index.ts
+++ plugins/AvatarDownloader/src/index.ts
@@ -1,37 +1,136 @@
-import { patcher } from "@vendetta";
 import { findByProps } from "@vendetta/metro";
-import React from "react";
-import DownloadAvatarButton from "./components/DownloadAvatarButton";
+import { before, after } from "@vendetta/patcher";
+import { React } from "@vendetta/metro/common";
+import { findInReactTree } from "@vendetta/utils";
+import { getAssetIDByName } from "@vendetta/ui/assets";
 
-const ProfileActions = findByProps("openUserProfileModal", "openUserProfile") ?? findByProps("showUserProfile");
-const UserProfileModal = findByProps("UserProfileModal") ?? findByProps("default", "UserProfile");
+const ActionSheet = findByProps("openLazy", "hideActionSheet");
+const { ActionSheetRow } = findByProps("ActionSheetRow");
 
-let unpatches: Function[] = [];
+const ClipboardUtils = findByProps("SUPPORTS_COPY", "copy");
+const ToastPresets = findByProps("presentCopiedToClipboard");
+const NativeLinking = findByProps("openURL", "canOpenURL");
+
+const DownloadIcon =
+    getAssetIDByName("ic_download_24px") ??
+    getAssetIDByName("DownloadIcon") ??
+    getAssetIDByName("download");
+
+let unpatches: (() => void)[] = [];
+
+function getAvatarUrl(author: any): string | null {
+    if (!author?.id) return null;
+    if (author.avatar) {
+        const isAnimated = author.avatar.startsWith("a_");
+        const ext = isAnimated ? "gif" : "png";
+        return `https://cdn.discordapp.com/avatars/${author.id}/${author.avatar}.${ext}?size=1024`;
+    }
+    const defaultIndex = Number((BigInt(author.id) >> 22n) % 6n);
+    return `https://cdn.discordapp.com/embed/avatars/${defaultIndex}.png`;
+}
+
+function handleAvatarDownload(author: any) {
+    const avatarUrl = getAvatarUrl(author);
+    if (!avatarUrl) return;
+
+    try {
+        if (ClipboardUtils?.copy) {
+            ClipboardUtils.copy(avatarUrl);
+        } else if (ClipboardUtils?.copyToClipboard) {
+            ClipboardUtils.copyToClipboard(avatarUrl);
+        }
+    } catch (e) {
+        console.error("[AvatarDownloader] Copy failed:", e);
+    }
+
+    if (NativeLinking?.openURL) {
+        NativeLinking.openURL(avatarUrl).catch(() => {});
+    }
+
+    ToastPresets?.presentCopiedToClipboard?.();
+}
 
 export default {
-  onLoad: () => {
-    if (UserProfileModal) {
-      const patch = patcher.after("default", UserProfileModal, (args, ret) => {
-        try {
-          const user = args[0]?.user;
-          if (!user) return ret;
+    onLoad() {
+        const unpatchOpenLazy = before(
+            "openLazy",
+            ActionSheet,
+            ([comp, args, msg]) => {
+                if (
+                    args !== "MessageLongPressActionSheet" ||
+                    !msg?.message
+                ) {
+                    return;
+                }
 
-          const children = ret?.props?.children;
-          if (Array.isArray(children)) {
-            children.push(React.createElement(DownloadAvatarButton, { user }));
-          }
-        } catch {}
-        return ret;
-      });
+                comp.then((instance: any) => {
+                    instance.__currentActiveMessage = msg.message;
 
-      unpatches.push(patch);
+                    if (instance.__patchedForAvatarDownload) return;
+                    instance.__patchedForAvatarDownload = true;
+
+                    const unpatchDefault = after(
+                        "default",
+                        instance,
+                        (_args: any, component: any) => {
+                            const groups: any[] = findInReactTree(
+                                component,
+                                (c: any) =>
+                                    Array.isArray(c) &&
+                                    c[0]?.type?.name === "ActionSheetRowGroup"
+                            );
+
+                            if (!groups?.length) return;
+
+                            const alreadyExists = findInReactTree(
+                                component,
+                                (c: any) => c?.props?.label === "Save Avatar Link"
+                            );
+                            if (alreadyExists) return;
+
+                            const avatarButton = React.createElement(
+                                ActionSheetRow,
+                                {
+                                    label: "Save Avatar Link",
+                                    icon: React.createElement(
+                                        ActionSheetRow.Icon,
+                                        { source: DownloadIcon }
+                                    ),
+                                    onPress: () => {
+                                        const currentAuthor =
+                                            instance.__currentActiveMessage?.author;
+
+                                        ActionSheet.hideActionSheet();
+
+                                        if (!currentAuthor) return;
+
+                                        setTimeout(() => {
+                                            handleAvatarDownload(currentAuthor);
+                                        }, 100);
+                                    }
+                                }
+                            );
+
+                            if (groups?.unshift) {
+                                groups.unshift(
+                                    React.createElement(ActionSheetRow.Group, null, avatarButton)
+                                );
+                            }
+                        }
+                    );
+
+                    unpatches.push(unpatchDefault);
+                });
+            }
+        );
+
+        unpatches.push(unpatchOpenLazy);
+    },
+
+    onUnload() {
+        for (const unpatch of unpatches) {
+            unpatch?.();
+        }
+        unpatches = [];
     }
-  },
-
-  onUnload: () => {
-    for (const unpatch of unpatches) {
-      unpatch();
-    }
-    unpatches = [];
-  }
 };
